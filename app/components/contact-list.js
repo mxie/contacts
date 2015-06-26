e133fbf2dd1eb4bd2210109156dd00b319ff15f9
Melissa Xie <melissa@thoughtbot.com>
2015-06-26 14:28:49 -0400

    Introduce searchable list of contacts

---
+++ app/components/contact-list.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  query: '',
+
+  filteredContacts: Ember.computed(
+    'query',
+    'contacts.@each.fullName',
+    function() {
+      const query = this.get('query');
+      const queryRegex = new RegExp(query, 'i');
+
+      return this.get('contacts').filter(function(contact) {
+        return contact.get('nameFirst'). match(queryRegex);
+      });
+    }
+  )
+});
